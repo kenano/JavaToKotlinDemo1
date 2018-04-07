80278570be7de3ecbee6b81d68039074acb9aa8e
Kenan Ozdamar <kenan.ozdamar@gmail.com>
2018-04-07 13:54:17 -0400

    Init project. Add dependencies needed in project.

---
+++ app/src/main/java/com/kenano/android/javatokotlindemo1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kenano.android.javatokotlindemo1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Description
+
+This project shows how Kotlin is configured in Android studio and how to convert 
+Java to Kotlin.
+
